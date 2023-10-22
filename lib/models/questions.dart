3a3ad687b00bea43201c3b69ee8c183afc35438b
MayankSachapara <137792267+MayankSachapara@users.noreply.github.com>
2023-10-22 15:34:42 +0530

    first

---
+++ lib/models/questions.dart
@@ -0,0 +1,110 @@
+import 'package:quiz_app_tutorial/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question:
+        'The Parliament of India cannot be regarded as a sovereign body because',
+    correctAnswerIndex: 3,
+    options: [
+      'a) it can legislate only on subjects entrusted to the Centre by the Constitution',
+      'b) it has to operate within the limits prescribed by the Constitution',
+      'c) the Supreme Court can declare laws passed by parliament as unconstitutional if they contravene the provisions of the Constitution',
+      'd) All of the above',
+    ],
+  ),
+  Question(
+    question: 'The percentage of irrigated land in India is about',
+    correctAnswerIndex: 2,
+    options: [
+      'a) 45',
+      'b) 65',
+      'c) 35',
+      'd) 25',
+    ],
+  ),
+  Question(
+    question:
+        'Nitrogen is fixed in ecosystems in ways stated below. Which one of the statements below is false?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) By cyanobacteria',
+      'b) By electrical discharges in the atmosphere',
+      'c) By industrially synthesised fertilizer',
+      'd) By denitrification',
+    ],
+  ),
+  Question(
+    question: 'The trident-shaped symbol of Buddhism does not represent',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Nirvana',
+      'b) Sangha',
+      'c) Buddha',
+      'd) Dhamma',
+    ],
+  ),
+  Question(
+    question:
+        'Which of the following places is famous for Chikankari work, which is a traditional art of embroidery?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Lucknow',
+      'b) Hyderabad',
+      'c) Jaipur',
+      'd) Mysore',
+    ],
+  ),
+  Question(
+    question:
+        "Which company created the most used networking software in the 1980's",
+    correctAnswerIndex: 3,
+    options: [
+      'a) Microsoft',
+      'b) Sun',
+      'c) IBM',
+      'd) Novell',
+    ],
+  ),
+  Question(
+    question: 'B. C. Roy Award is given in the field of',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Music',
+      'b) Journalism',
+      'c) Medicine',
+      'd) Environment',
+    ],
+  ),
+  Question(
+    question:
+        'Which was the 1st non Test playing country to beat India in an international match?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Canada',
+      'b) Sri Lanka',
+      'c) Zimbabwe',
+      'd) East Africa',
+    ],
+  ),
+  Question(
+    question:
+        'Who headed the first scientific group to leave for Antarctica in 1982?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Dr. D.R. Sengupta',
+      'b) Dr. S.Z. Kasim',
+      'c) Dr. V. K. Raina',
+      'd) Dr. H. K. Gupta',
+    ],
+  ),
+  Question(
+    question: 'Which of the following is not associated with the UNO?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) ILO',
+      'b) WHO',
+      'c) ASEAN',
+      'd) All of the above',
+    ],
+  ),
+];
